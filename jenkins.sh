f7410b3c1f0d82fb9f99fa7df6d588128635b993
emirsway <emirmails@gmail.com>
2021-11-08 00:05:09 +0000

    updated jenkins.sh with the correct package

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,10 @@
 # #!/bin/bash
-# sudo yum install wget -y
-# sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+# sudo yum install -y jenkins java-11-openjdk-devel
+# sudo yum -y install wget
+# sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 # sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-# sudo yum upgrade -y 
-# sudo yum install epel-release java-11-openjdk-devel -y
+# sudo yum upgrade -y
+# sudo yum install epel-release -y
 # sudo yum install jenkins -y
-# sudo systemctl daemon-reload
 # sudo systemctl start jenkins
-# sudo systemctl enable jenkins
-# systemctl status jenkins
+# sudo systemctl status jenkins
