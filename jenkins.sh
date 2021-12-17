fd4afc015158dbc3dbb1fcc7bcdf68e98460276c
Emir Koroglu <69024539+emirsway@users.noreply.github.com>
2021-12-17 16:27:57 -0600

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,22 @@
 # #!/bin/bash
-# sudo yum install -y jenkins java-11-openjdk-devel
-# sudo yum -y install wget
-# sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-# sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-# sudo yum upgrade -y
-# sudo yum install epel-release -y
-# sudo yum install jenkins -y
-# sudo systemctl start jenkins
-# sudo systemctl status jenkins
+sudo yum install wget -y
+
+sudo yum install git -y
+
+sudo yum install unzip -y
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum upgrade -y
+
+sudo yum install epel-release java-11-openjdk-devel -y
+
+sudo yum install jenkins -y
+
+sudo systemctl daemon-reload
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
